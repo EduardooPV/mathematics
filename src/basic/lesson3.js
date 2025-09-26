684762c61d676552e1ab78951dc45706e159f84e
EduardooPV <eduardoveltroni@hotmail.com>
2025-09-26 13:21:27 -0300

    feat: more ex lesson3

--- src/basic/lesson3.js
+++ src/basic/lesson3.js
@@ -5,3 +5,25 @@ const multiply = (firstNumber, secondNumber) => {
 const result = multiply(4, 6);
 
 console.log(result);
+
+const multiplyMany = (...params) => {
+  let result = 1;
+
+  for (i = 0; i < params.length; i++) {
+    result = result * params[i];
+  }
+
+  return result;
+};
+
+const result1 = multiplyMany(2, 3, 4);
+
+console.log(result1);
+
+const double = (number) => {
+  return multiply(number, 2);
+};
+
+const result2 = double(7);
+
+console.log(result2);
