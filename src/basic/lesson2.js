fda6c73e565978e96767ad1cb12b83167f19e40d
EduardooPV <eduardoveltroni@hotmail.com>
2025-09-26 13:10:45 -0300

    feat: more ex lesson 2

--- src/basic/lesson2.js
+++ src/basic/lesson2.js
@@ -5,3 +5,25 @@ const subtract = (firstNumber, secondNumber) => {
 const result = subtract(5, 3);
 
 console.log(result);
+
+const absoluteDifference = (firstNumber, secondNumber) => {
+  return Math.abs(firstNumber - secondNumber);
+};
+
+const result1 = absoluteDifference(3, 5);
+
+console.log(result1);
+
+const subtractMany = (...params) => {
+  let result = params[0];
+
+  for (number of params.slice(1)) {
+    result = result - number;
+  }
+
+  return result;
+};
+
+const result2 = subtractMany(20, 5, 3);
+
+console.log(result2);
