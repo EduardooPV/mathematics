5278d86e70ae6dd4f0d55791b6efacffc65570ec
EduardooPV <eduardoveltroni@hotmail.com>
2025-09-26 13:03:38 -0300

    feat: more ex lesson 1

--- src/basic/lesson1.js
+++ src/basic/lesson1.js
@@ -5,3 +5,22 @@ const add = (firstNumber, secondNumber) => {
 const result = add(2, 3);
 
 console.log(result);
+
+const addMany = (...params) => {
+  const total = params.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  return total;
+};
+
+const result2 = addMany(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+console.log(result2);
+
+const increment = (number) => {
+  return number + 1;
+};
+
+const result3 = increment(9);
+
+console.log(result3);
